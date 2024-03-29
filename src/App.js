f7cd361c667afb32e59ebdf4ea388b5a248f4071
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2024-03-29 23:40:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-
+import React from 'react';
 import './App.css';
-import TodoForm from './componant/TodoForm';
-import TodoList from './componant/TodoList';
-
-
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="todo-app">
-      <TodoList/>
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
